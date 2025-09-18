3fc81aa1a7f4e82ca8c941d21d3ef407c48e4011
Harry Yu <yupakhei456@gmail.com>
2025-09-18 15:11:30 -0400

    Add init db script

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,14 +1,57 @@
+import { auth } from "@/lib/auth";
 import { db } from ".";
-import { taccounts } from "./schema";
+import { entries, taccounts, transactions } from "./schema";
 
 async function main() {
-  await db.insert(taccounts).values([
+  const user = await auth.api.signUpEmail({
+    body: {
+      email: "m@example.com",
+      name: "John Doe",
+      password: "12345678",
+    },
+  });
+  console.info("User created");
+  const accounts = await db
+    .insert(taccounts)
+    .values([
+      {
+        name: "Opening balance",
+        normalSide: "cr",
+        type: "equity",
+        userId: user.user.id,
+      },
+      {
+        name: "Cash",
+        normalSide: "dr",
+        type: "asset",
+        userId: user.user.id,
+      },
+    ])
+    .returning();
+  console.info("Accounts added");
+  const transaction = await db
+    .insert(transactions)
+    .values({
+      date: new Date(),
+      userId: user.user.id,
+      description: "Opening balance for cash",
+    })
+    .returning();
+  await db.insert(entries).values([
     {
-      name: "Grocery",
-      normalSide: "dr",
-      type: "expense",
+      amount: 10,
+      side: "dr",
+      taccountId: accounts.find((ac) => ac.name === "Cash")?.id ?? 0,
+      transactionId: transaction[0].id,
+    },
+    {
+      amount: 10,
+      side: "cr",
+      taccountId: accounts.find((ac) => ac.name === "Opening balance")?.id ?? 0,
+      transactionId: transaction[0].id,
     },
   ]);
+  console.info("Transaction added");
   console.info("DB Initialized");
 }
 
