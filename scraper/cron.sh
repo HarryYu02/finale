6a0d4248e08bc4fea556de056c43be9ae02612ee
Harry Yu <yupakhei456@gmail.com>
2025-09-28 23:13:21 -0400

    Scraper auto scrape all tickers

--- scraper/cron.sh
+++ scraper/cron.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 dir=$(pwd)
+chmod +x ${dir}/scraper/curl.sh
 crontab -l > crontab_new
 echo "0 0 * * * cd ${dir} && ${dir}/scraper/curl.sh" >> crontab_new
 crontab crontab_new
 rm crontab_new
+
--- scraper/curl.sh
+++ scraper/curl.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
 if [[ $# -eq 1 ]]; then
-  ticker=$1
+  tickers=$@
 else
-  ticker="VFV:TSE"
+  tickers=$(sqlite3 sqlite.db "select distinct ticker from stock_prices;")
 fi
 
 date=$(date)
 name="$(echo ${ticker} | tr ":\-" "_")"
 
-curl -o scraper/${name}.txt "https://www.google.com/finance/quote/${ticker}"
-printf "\n${date}" >> scraper/${name}.txt
-price=$(rg data-last-price scraper/${name}.txt | sed -E 's/.*data-last-price=[^\$]*\$([0-9,]+\.[0-9]+).*/\1/' | tr -d ',')
-currency=$(rg data-currency-code scraper/${name}.txt | sed -E 's/.*data-currency-code=\"([a-zA-Z]+)\".*/\1/')
+for ticker in ${tickers[@]}; do
+  curl -o scraper/${name}.txt "https://www.google.com/finance/quote/${ticker}"
+  printf "\n${date}" >> scraper/${name}.txt
+  price=$(rg data-last-price scraper/${name}.txt | sed -E 's/.*data-last-price=[^\$]*\$([0-9,]+\.[0-9]+).*/\1/' | tr -d ',')
+  currency=$(rg data-currency-code scraper/${name}.txt | sed -E 's/.*data-currency-code=\"([a-zA-Z]+)\".*/\1/')
 
-if [[ "${#price}" -ge 1 ]]; then
-  echo "${date}: SUCCESS" >> scraper/log.txt
-  pnpm tsx scraper/insert.ts "${date}" "${ticker}" "${currency}" "${price}" "google_finance"
-else
-  echo "${date}: ERROR" >> scraper/log.txt
-  echo "ERROR: Invalid ticker symbol"
+  if [[ "${#price}" -ge 1 ]]; then
+    echo "${date}: [${ticker}] SUCCESS" >> scraper/log.txt
+    pnpm tsx scraper/insert.ts "${date}" "${ticker}" "${currency}" "${price}" "google_finance"
+  else
+    echo "${date}: [${ticker}] ERROR" >> scraper/log.txt
+    echo "ERROR: Invalid ticker symbol"
+  fi
   rm scraper/${name}.txt
-  exit 1
-fi
+  sleep 5
+done
 
 exit 0
-
