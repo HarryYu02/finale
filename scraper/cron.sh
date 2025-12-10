bcb220a79eef19fdc88d2a761d633de0fbfc4cc9
Harry Y. <harryyu02@gmail.com>
2025-12-10 01:55:09 -0500

    fix scraper cron init

--- scraper/cron.sh
+++ scraper/cron.sh
@@ -3,7 +3,7 @@
 dir=$(pwd)
 chmod +x ${dir}/scraper/curl.sh
 crontab -l > crontab_new
-echo "0 0 * * * cd ${dir} && ${dir}scraper/curl.sh" >> crontab_new
+echo "0 0 * * * cd ${dir} && ${dir}/scraper/curl.sh" >> crontab_new
 crontab crontab_new
 rm crontab_new
 
